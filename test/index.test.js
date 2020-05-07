66dbc7c3ca286274aa164a6683578525c7da8897
exuanbo <exuanbo@protonmail.com>
2020-05-07 23:16:17 +0800

    test: add test

---
+++ test/index.test.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const fs = require('fs')
+const should = require('should')
+const Vinyl = require('vinyl')
+const lqipBase64 = require('..')
+
+describe('gulp-lqip-base64', () => {
+  it('Should transform images in test.html, making it equal to expected.html', done => {
+    const testHtml = new Vinyl({
+      base: 'test/',
+      path: 'test/test.html',
+      contents: fs.readFileSync('test/test.html')
+    })
+
+    const expectedHtml = new Vinyl({
+      base: 'test/',
+      path: 'test/expected.html',
+      contents: fs.readFileSync('test/expected.html')
+    })
+
+    const stream = lqipBase64()
+
+    stream.on('error', error => {
+      should.exist(error)
+      done(error)
+    })
+
+    stream.on('data', file => {
+      should.exist(file)
+      should.exist(file.contents)
+
+      const contents = String(file.contents)
+      should.equal(contents, String(expectedHtml.contents))
+      done()
+    })
+
+    stream.write(testHtml)
+    stream.end()
+  })
+})
